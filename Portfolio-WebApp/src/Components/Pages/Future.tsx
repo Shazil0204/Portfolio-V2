cf146aa04369a08cb9cf9ae17a923ca1af446bfa
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-08-23 10:00:45 +0200

    Basic structure is ready
    Tailwind css
    React Scroll
    files i need, tsx and {ts for styling}

---
+++ Portfolio-WebApp/src/Components/Pages/Future.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Future = () => {
+  return (
+    <div>Future</div>
+  )
+}
---
+++ Portfolio-WebApp/src/Components/Styles/Navbar.ts
@@ -0,0 +1,9 @@
+// This is just to show how it is going to work
+
+const styles = {
+  container: "bg-blue-500 h-lvh w-lvw",
+  button: "bg-green-500 p-4 rounded",
+  header: "text-2xl font-bold",
+};
+
+export default styles;
